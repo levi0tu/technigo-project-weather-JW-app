d716bdc8dafa370a6164c1dfe2366a085c888502
Jeanette Wiktorsson <leiia@hotmail.com>
2026-01-28 10:53:35 +0100

    Uppdaterat api och lagt till fler värden

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+const city = document.getElementById("city");
 const weatherElement = document.getElementById("weather");
-
 const sunrise = document.getElementById("sunrise");
 const sunset = document.getElementById("sunset");
 const temp = document.getElementById("temp");
 
-fetch("https://api.openweathermap.org/data/2.5/weather?id=524901&appid=2665d65d0763ec2a1c36cbf60a19e7fc")
+//Hämtar väder i Kungsbacka i metrics
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Kungsbacka,SE&appid=2665d65d0763ec2a1c36cbf60a19e7fc&units=metric")
     .then(response => response.json())
-    .then(data => weatherElement.textContent = data.weather[0].main);
+    .then(data => {
+        console.log(data);
+        city.textContent = (data.name);
+        weatherElement.textContent = (data.weather[0].main);
 
+        //sunrise och sunset behöver konverteras till tid enligt api
+        const sunriseTime = new Date(data.sys.sunrise * 1000);
+        const sunsetTime = new Date(data.sys.sunset * 1000);
+        //slice klipper ut positionen som innehåller tiden jag är ute efter
+        sunrise.textContent = sunriseTime.toUTCString().slice(17, 22);
+        sunset.textContent = sunsetTime.toUTCString().slice(17, 22);
+
+        //rundar av temperaturen och lägger till gradtecken
+        temp.textContent = Math.round(data.main.temp) + "°C";
+    });
